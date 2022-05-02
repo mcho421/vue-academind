9d52fea56e4401f9e8ee237df9abc158b8d4b289
Mathew Chong <mathewchong.dev@gmail.com>
2022-05-03 09:06:07 +1000

    Add header and card styling

--- prj-cmp/src/App.vue
+++ prj-cmp/src/App.vue
@@ -1,20 +1,14 @@
 <template>
-  <ul>
-    <LearningResource
-      v-for="res in storedResources"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :link="res.link"
-    />
-  </ul>
+  <TheHeader title="RememberMe" />
+  <StoredResources :resources="storedResources" />
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
-  components: { LearningResource },
+  components: { StoredResources, TheHeader },
   data() {
     return {
       storedResources: [
@@ -35,3 +29,19 @@ export default {
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
